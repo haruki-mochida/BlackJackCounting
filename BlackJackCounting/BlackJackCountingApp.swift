6f0d2c24313c129b58134e75f26f57100a2568c8
mochidaharuki <mochidaharuki@plise.co.jp>
2023-04-20 11:37:28 +0900

    Initial Commit

---
+++ BlackJackCounting/BlackJackCountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackCountingApp.swift
+//  BlackJackCounting
+//
+//  Created by 持田晴生 on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackCountingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
